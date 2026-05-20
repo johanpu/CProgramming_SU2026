c7f3f8ff6bcb7e3a92d85359370ff1b71693de56
Johan A. Puerta Santos <169589868+johanpu@users.noreply.github.com>
2026-05-20 18:15:00 +0000

    Create first program.

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,16 @@
+/*
+    05/20/2026.
+*/
+
+// Preprocessor directive (starts with #)
+#include <stdio.h> // stdio - standard input output, .h - header file.
+
+// Create the main function.
+// <return type> <name> (<parameters>)
+int main(void) {
+    printf("Welcome\nto\nCOP2271!\n"); // \n - new line (escape character)
+    printf("Are\nyou\nready\nto\ncode?\n");
+
+    return 0;
+}
+
